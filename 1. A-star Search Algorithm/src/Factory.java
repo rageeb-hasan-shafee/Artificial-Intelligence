a20282b532548131124609ca45558cf957b4df82
hunter <2105175@ugrad.cse.buet.ac.bd>
2025-06-20 02:40:34 +0600

    Re-add 'src' folder as a regular directory

---
+++ 1. A-star Search Algorithm/src/Factory.java
@@ -0,0 +1,13 @@
+public class Factory {
+    public static Heuristic getHeuristic(String type) {
+        if (type == null) return null;
+
+        switch (type.toUpperCase()) {
+            case "MANHATTAN": return new ManhattanHeuristic();
+            case "HAMMING": return new HammingHeuristic();
+            case "EUCLIDEAN": return new EuclideanHeuristic();
+            case "LINEAR_CONFLICT": return new LinearConflictHeuristic();
+            default: return null;
+        }
+    }
+}
